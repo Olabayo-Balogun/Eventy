1953db3a87949d46c02185352518e6a87bd4afb8
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-04-07 21:26:50 +0100

    Add project files.

---
+++ Controllers/MaterialsController.cs
@@ -0,0 +1,19 @@
+﻿using Eventy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eventy.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        // GET: Materials/Random
+        public ActionResult Random()
+        {
+            var material = new Material() { Name = "Pot" };
+            return View(material);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eventy.MvcApplication" Language="C#" %>
