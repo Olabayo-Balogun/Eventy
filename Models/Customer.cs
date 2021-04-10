a992e844d91896e0e7504797a23ccd44d413b83c
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-04-10 11:56:43 +0100

    Added data annotation to customer class

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,23 @@ namespace Eventy.Models
     public class Customer
     {
         public int Id { get; private set; }
+        [Required]
+        [StringLength (50)]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
         public string LastName { get; set; }
+        [Required]
         public DateTime DateOfBirth { get; set; }
+        public int Age { get { return ((DateTime.Today - DateOfBirth).Days)/365; } }
+        [Required]
+        [StringLength(14)]
         public string PhoneNumber { get; set; }
+        [Required]
+        [StringLength(100)]
         public string EmailAddress { get; set; }
+        [Required]
+        [StringLength(250)]
         public string HomeAddress { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public string Image { get; set; }
